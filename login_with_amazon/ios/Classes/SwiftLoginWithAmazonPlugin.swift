399ee1f62611bea44c1885a99bd77d08cf9c888c
Bradley <wpadmin@WePlenishs-Air.home.local>
2020-03-11 00:37:19 -0400

    ios nonsense

--- login_with_amazon/ios/Classes/SwiftLoginWithAmazonPlugin.swift
+++ login_with_amazon/ios/Classes/SwiftLoginWithAmazonPlugin.swift
@@ -1,31 +1,34 @@
 import Flutter
 import UIKit
 
-func mapToScope(key:String, value: Any) -> AMZNScope{
-  switch type(of: value) {
-  case is Dictionary<AnyHashable, AnyHashable>.Type:
-    return AMZNScopeFactory.scope(withName: key, data: value as! [AnyHashable : Any])
-  default:
-    return AMZNScopeFactory.scope(withName: value as! String)
+func dictToHashable(dict: [Any : Any]) throws -> [AnyHashable : Any]{
+  let hashableDict: [AnyHashable : Any] = [:]
+  for (key, value) in dict {
+    if let hashedKey = key as? AnyHashable {
+      if let dictVal = value as? [Any : Any]{
+        hashableDict[hashedKey] = try dictToHashable(dict: dictVal)
+      }else{
+        hashableDict[hashedKey] = value
+      }
+    }else{
+      throw ArgsError.ScopeFailure
+    }
   }
+  return hashableDict
 }
 
-func createScope(dict: Dictionary<String,Any>) -> [AMZNScope]{
-  return dict.map(mapToScope)
+func mapToScope(key:String, value: Any) throws -> AMZNScope{
+    if let data = value as? [Any : Any] {
+        return AMZNScopeFactory.scope(withName: key, data: try dictToHashable(dict: data))
+    }else if let name = value as? String {
+        return AMZNScopeFactory.scope(withName: name)
+    } else {
+        throw ArgsError.ScopeFailure
+    }
 }
 
-func getArguments(arguments: Any?) -> [String: Any]{
-  guard let args = arguments as? [String : Any] else {
-    return [:]
-  }
-  return args
-}
-
-func getScopes(args: [String: Any]) -> Dictionary<String, Any> {
-  guard let scopes : Dictionary = args["scopes"] as? Dictionary<String, Any> else {
-    return [:]
-  }
-  return scopes
+func createScope(dict: [String: Any]) throws -> [AMZNScope]{
+  return try dict.map(mapToScope)
 }
 
 func ReqHandler(result: @escaping FlutterResult) -> AMZNAuthorizationRequestHandler {
@@ -47,19 +50,31 @@ func ReqHandler(result: @escaping FlutterResult) -> AMZNAuthorizationRequestHand
   }
 }
 
-func AMZNReqFromFlutterArgs(arguments: Any?) -> AMZNAuthorizeRequest {
-  let args = getArguments(arguments: arguments)
-  let scopes = getScopes(args: args)
-  let req = AMZNAuthorizeRequest.init()
-  req.scopes = createScope(dict: scopes)
-  
-  if(args["codeChallenge"] != nil){
-    req.codeChallenge = args["codeChallenge"] as! String
-  }
-  if(args["codeChallengeMethod"] != nil){
-    req.codeChallengeMethod = args["codeChallengeMethod"] as! String
-  }
-  return req
+enum ArgsError: Error {
+    case MissingArgs
+    case ScopeFailure
+}
+
+func AMZNReqFromFlutterArgs(arguments: Any?) throws -> AMZNAuthorizeRequest {
+    if let args = arguments as? [String: Any],
+    let scopes = args["scopes"] as? [String: Any] {
+      let req = AMZNAuthorizeRequest.init()
+      do{
+        req.scopes = try createScope(dict: scopes)
+      }catch {
+        throw ArgsError.ScopeFailure
+      }
+      
+      if(args["codeChallenge"] != nil){
+        req.codeChallenge = args["codeChallenge"] as! String
+      }
+      if(args["codeChallengeMethod"] != nil){
+        req.codeChallengeMethod = args["codeChallengeMethod"] as! String
+      }
+      return req
+    }else{
+        throw ArgsError.MissingArgs
+    }
 }
 
 public class SwiftLoginWithAmazonPlugin: NSObject, FlutterPlugin {
@@ -70,40 +85,48 @@ public class SwiftLoginWithAmazonPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-      case "login":
-        //let args = getArguments(arguments: call.arguments)
-        //let scopes = getScopes(args: args)
-        //AIMobileLib.authorizeUser(forScopes: createScope(dict: scopes), delegate: self)
-        
-        let req = AMZNReqFromFlutterArgs(arguments: call.arguments)
-        req.grantType = AMZNAuthorizationGrantType.token
-        AMZNAuthorizationManager.shared().authorize(req, withHandler: ReqHandler(result: result))
-        return
-      case "getAuthCode":
-        let req = AMZNReqFromFlutterArgs(arguments: call.arguments)
-        req.grantType = AMZNAuthorizationGrantType.code
-        AMZNAuthorizationManager.shared().authorize(req, withHandler: ReqHandler(result: result))
-        return
-      case "logout":
-        AMZNAuthorizationManager.shared().signOut { (error) in
-            if((error) != nil) {
-                result(FlutterError(code: "Signout Failure", message: "User could not sign out", details: error))
-            }else{
-                //AIMobileLib.clearAuthorizationState(self)
-                result(true)
+    do {
+        switch call.method {
+          case "login":
+            //let args = getArguments(arguments: call.arguments)
+            //let scopes = getScopes(args: args)
+            //AIMobileLib.authorizeUser(forScopes: createScope(dict: scopes), delegate: self)
+            
+            let req = try AMZNReqFromFlutterArgs(arguments: call.arguments)
+            req.grantType = AMZNAuthorizationGrantType.token
+            AMZNAuthorizationManager.shared().authorize(req, withHandler: ReqHandler(result: result))
+            return
+          case "getAuthCode":
+            let req = try AMZNReqFromFlutterArgs(arguments: call.arguments)
+            req.grantType = AMZNAuthorizationGrantType.code
+            AMZNAuthorizationManager.shared().authorize(req, withHandler: ReqHandler(result: result))
+            return
+          case "logout":
+            AMZNAuthorizationManager.shared().signOut { (error) in
+                if((error) != nil) {
+                    result(FlutterError(code: "Signout Failure", message: "User could not sign out", details: error))
+                }else{
+                    //AIMobileLib.clearAuthorizationState(self)
+                    result(true)
+                }
             }
+            return
+          case "getAccessToken":
+            result(FlutterMethodNotImplemented)
+            //let args = getArguments(arguments: call.arguments)
+            //let scopes = getScopes(args: args)
+            //AIMobileLib.getAccessToken(forScopes: createScope(dict: scopes), withOverrideParams: nil, delegate: self)
+            return
+          default:
+            result(FlutterMethodNotImplemented)
+            return
         }
-        return
-      case "getAccessToken":
-        result(FlutterMethodNotImplemented)
-        //let args = getArguments(arguments: call.arguments)
-        //let scopes = getScopes(args: args)
-        //AIMobileLib.getAccessToken(forScopes: createScope(dict: scopes), withOverrideParams: nil, delegate: self)
-        return
-      default:
-        result(FlutterMethodNotImplemented)
-        return
+    } catch ArgsError.MissingArgs {
+      result(FlutterError(code: "Missing Arguments", message: "Scopes required", details: nil))
+    } catch ArgsError.ScopeFailure {
+      result(FlutterError(code: "Bad Scopes", message: "Scopes are malformed", details: nil))
+    } catch {
+      result(FlutterError(code: "Something went wrong", message: ":(", details: nil))
     }
   }
 }
